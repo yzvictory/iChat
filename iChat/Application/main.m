0efe61c206e28688531d6ed3ee7550000ac7387c
yangzhen <1784054000@qq.com>
2015-09-09 16:34:14 +0800

    初始化项目

---
+++ iChat/Application/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iChat
+//
+//  Created by yz on 15/9/9.
+//  Copyright (c) 2015年 DeviceOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
