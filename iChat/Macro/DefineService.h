ad724f8ffa4f7c616fcf79ac41877e59cf44e714
yangzhen <1784054000@qq.com>
2015-09-13 10:57:34 +0800

    添加storyboard

---
+++ iChat/Macro/DefineService.h
@@ -0,0 +1,15 @@
+//
+//  DefineService.h
+//  iChat
+//
+//  Created by yz on 15/9/13.
+//  Copyright (c) 2015年 DeviceOne. All rights reserved.
+//
+
+#ifndef iChat_DefineService_h
+#define iChat_DefineService_h
+
+#define kServiceUUID @"B58EC483-1F3B-4386-9994-7656F7CD16F7" //服务uuid
+#define kCharacterUUID @"7D1EF0CE-3162-4495-94D0-D3137837B60F" //特征uuid
+
+#endif
